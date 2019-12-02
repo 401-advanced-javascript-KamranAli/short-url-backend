49fc12c6d7b53c55b19af7ea0afd52b2237fbce4
Kamran Ali <kgali21@gmail.com>
2019-12-02 11:24:19 -0800

    changed some files around, got caught up

--- server.js
+++ server.js
@@ -1,16 +1,8 @@
-// loads the .env environment variables
 require('dotenv').config();
-// connect to mongo
-require('./lib/connect')(process.env.MONGODB_URI);
-
-// require the app http event handler
+require('./lib/connect')();
 const app = require('./lib/app');
-// create an http server that uses app
-const { createServer } = require('http');
-const server = createServer(app);
-
-// start the server by listening on a port
-const PORT = process.env.PORT || 3000;
-server.listen(PORT, () => {
-  console.log(`Listening on ${PORT}`);
+const PORT = process.env.PORT || 7890;
+app.listen(PORT, () => {
+  // eslint-disable-next-line no-console
+  console.log(`Started on ${PORT}`);
 });
